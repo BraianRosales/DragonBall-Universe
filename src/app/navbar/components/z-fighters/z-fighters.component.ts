abac065a47fd4c66994d434a7638cf2b6ba091a3
BraianRosales <braianezequielrosales@gmail.com>
2022-08-23 21:46:06 -0300

    realice la petición en la vista zFighters

--- src/app/navbar/components/z-fighters/z-fighters.component.ts
+++ src/app/navbar/components/z-fighters/z-fighters.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterDB } from '../../../interfaces/index';
+import { DragonballService } from '../../../services/dragonball.service';
 
 @Component({
   selector: 'app-z-fighters',
   templateUrl: './z-fighters.component.html',
-  styleUrls: ['./z-fighters.component.css']
+  styleUrls: ['./z-fighters.component.css'],
 })
 export class ZFightersComponent implements OnInit {
+  listZ: CharacterDB[] = [];
 
-  constructor() { }
+  constructor(private dragonballService: DragonballService) {}
 
   ngOnInit(): void {
+    this.dragonballService.charactersDB().subscribe(characters => {
+      this.listZ = characters.filter((character:CharacterDB) => character.role === 'Guerrero Z')
+    })
   }
-
 }
--- src/app/navbar/components/z-fighters/z-fighters.module.ts
+++ src/app/navbar/components/z-fighters/z-fighters.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [ZFightersComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class ZFightersModule {}
