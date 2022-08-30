e1345afc72b77840153cc85578bf85e0cf9e54dc
BraianRosales <braianezequielrosales@gmail.com>
2022-08-30 00:07:42 -0300

    refactor api_url

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CharacterDB } from '../interfaces/index';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://dragon-ball-super-api.herokuapp.com/api',
 };
